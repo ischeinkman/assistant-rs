83a4cfb5e42b37f5d5ce955cb07cbdac3daf6218
ischeinkman <scheinkman.ilan@gmail.com>
2020-07-13 15:34:36 -0700

    Added config commandline flag.

---
+++ src/context.rs
@@ -0,0 +1,114 @@
+use crate::buffer::{AudioReciever, SpeechLoader};
+use crate::config;
+use crate::config::{Command, Config};
+use crate::error::AssistantRsError;
+use crate::metrics;
+use crate::speech::Utterance;
+
+use cpal::traits::HostTrait;
+use deepspeech::dynamic::Model;
+
+use std::path::PathBuf;
+use std::process;
+use std::time::Duration;
+
+pub struct AssistantContext {
+    model: Model,
+    config: Config,
+    config_paths: Vec<PathBuf>,
+}
+
+impl AssistantContext {
+    pub fn init_from_paths(config_paths: Vec<PathBuf>) -> Result<Self, AssistantRsError> {
+        let config = config::cascade_configs(&config_paths)?;
+        config.verify()?;
+        let model = build_model(&config)?;
+        Ok(Self {
+            model,
+            config,
+            config_paths,
+        })
+    }
+    pub fn reload(&mut self) -> Result<(), AssistantRsError> {
+        let new_conf = config::cascade_configs(&self.config_paths)?;
+        if self.config != new_conf {
+            let new_model = build_model(&new_conf)?;
+            self.model = new_model;
+            self.config = new_conf;
+        }
+        Ok(())
+    }
+    fn build_audio_stream(&mut self) -> Result<AudioReciever, AssistantRsError> {
+        let sample_rate = self.model.get_sample_rate();
+        let host = cpal::default_host();
+        let dev = host
+            .default_input_device()
+            .ok_or(AssistantRsError::MicrophoneNotFound)?;
+        let stream_conf = cpal::StreamConfig {
+            channels: 1,
+            sample_rate: cpal::SampleRate(sample_rate as u32),
+        };
+        AudioReciever::construct(&dev, &stream_conf)
+    }
+    fn run_command(&self, cmd: &Command) -> Result<(), AssistantRsError> {
+        let raw_command = cmd.command();
+        process::Command::new("sh")
+            .arg("-c")
+            .arg(raw_command)
+            .stderr(process::Stdio::null())
+            .stdout(process::Stdio::null())
+            .stdin(process::Stdio::null())
+            .spawn()?;
+        Ok(())
+    }
+    pub fn run(&mut self) -> Result<(), AssistantRsError> {
+        let sample_rate = self.model.get_sample_rate();
+        let audio_recv = self.build_audio_stream()?;
+        let strm = self.model.create_stream()?;
+        let mut loader = SpeechLoader::new(strm, sample_rate as u32);
+        loop {
+            if loader.time_since_change() > Duration::from_millis(100)
+                && !loader.current_text().is_empty()
+            {
+                break;
+            }
+            let l = audio_recv.wait_until(sample_rate as usize)?;
+            loader.push(&l)?;
+        }
+        println!("======== LOADER DEINIT ==============");
+        let final_samples = loader.num_samples();
+        let (final_msg, _) = loader.finish();
+        let final_msg = final_msg.trim();
+        let phones = Utterance::parse_with_unknowns(final_msg);
+        println!("-> Sample count: {}", final_samples);
+        println!("-> Final text: {}", final_msg);
+        println!("-> Final phones: {:?}", phones);
+        let (cmd, d) = self
+            .config
+            .commands
+            .iter()
+            .map(|cmd| {
+                let dl = metrics::leven_dist(cmd.message(), final_msg);
+                println!("= = > CMD {}: {}", cmd.message(), dl);
+                (cmd, dl)
+            })
+            .min_by(|(_, da), (_, db)| da.partial_cmp(db).unwrap())
+            .unwrap();
+        println!("= > Choice: {}, distance {}", cmd.message(), d);
+        self.run_command(cmd)?;
+        Ok(())
+    }
+}
+
+fn build_model(conf: &Config) -> Result<Model, AssistantRsError> {
+    let lib = conf.deepspeech_config.library_path()?;
+    let model = conf.deepspeech_config.model_path()?;
+    let mut retvl = Model::load_from_files(lib.as_ref(), model.as_ref())?;
+    if let Some(scorer) = conf.deepspeech_config.scorer_path()? {
+        retvl.enable_external_scorer(scorer)?;
+    }
+    if let Some(w) = conf.deepspeech_config.beam_width()? {
+        retvl.set_model_beam_width(w)?;
+    }
+    Ok(retvl)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,75 @@
 mod buffer;
 mod config;
+mod context;
 mod error;
 mod metrics;
 mod speech;
 mod utils;
 
-use buffer::SpeechLoader;
-use config::{Command, Config};
+use structopt::StructOpt;
 
-use cpal::traits::HostTrait;
-use deepspeech::dynamic::Model;
 use std::path::PathBuf;
-use std::process;
-use std::time::Duration;
+
+use crate::utils::{IterUtils, StringUtils};
 
 fn main() {
-    let paths = config::get_config_dirs().unwrap();
-    let mut ctx = AssistantContext::init_from_paths(paths).unwrap();
+    let args = Args::from_args();
+    let arg_confs = args.configs.into_iter();
+    let paths = arg_confs
+        .chain(get_xdg_config_files().into_iter())
+        .collect();
+    let mut ctx = crate::context::AssistantContext::init_from_paths(paths).unwrap();
     ctx.run().unwrap();
 }
 
-fn build_model(conf: &Config) -> Result<Model, error::AssistantRsError> {
-    let lib = conf.deepspeech_config.library_path()?;
-    let model = conf.deepspeech_config.model_path()?;
-    let mut retvl = Model::load_from_files(lib.as_ref(), model.as_ref())?;
-    if let Some(scorer) = conf.deepspeech_config.scorer_path()? {
-        retvl.enable_external_scorer(scorer)?;
-    }
-    if let Some(w) = conf.deepspeech_config.beam_width()? {
-        retvl.set_model_beam_width(w)?;
-    }
-    Ok(retvl)
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "AssistantRS",
+    about = "A simple, configurable, and offline voice assistant."
+)]
+pub struct Args {
+    /// Extra config files to read from.
+    #[structopt(name = "config", long = "config")]
+    configs: Vec<PathBuf>,
 }
 
-pub struct AssistantContext {
-    model: Model,
-    config: Config,
-    config_paths: Vec<PathBuf>,
+pub fn get_xdg_config_files() -> Vec<PathBuf> {
+    let mut retvl = Vec::new();
+    let xdg_config_home = xdg_home_config_dir();
+    let xdg_config_dirs = xdg_sys_config_dirs();
+    let xdg_files = xdg_config_home
+        .into_iter()
+        .chain(xdg_config_dirs)
+        .map(config_root_to_toml);
+    retvl.extend(xdg_files);
+    retvl
 }
 
-impl AssistantContext {
-    pub fn init_from_paths(config_paths: Vec<PathBuf>) -> Result<Self, error::AssistantRsError> {
-        let config = config::cascade_configs(&config_paths)?;
-        config.verify()?;
-        let model = build_model(&config)?;
-        Ok(Self {
-            model,
-            config,
-            config_paths,
+fn config_root_to_toml(mut pt: PathBuf) -> PathBuf {
+    pt.push("assistant-rs");
+    pt.push("assistant.toml");
+    pt
+}
+
+fn xdg_home_config_dir() -> Option<PathBuf> {
+    std::env::var_os("XDG_CONFIG_HOME")
+        .map(|s| PathBuf::from(s))
+        .or_else(|| {
+            let home = std::env::var_os("HOME")?;
+            let mut pt = PathBuf::from(home);
+            pt.push(".config");
+            Some(pt)
         })
-    }
-    pub fn reload(&mut self) -> Result<(), error::AssistantRsError> {
-        let new_conf = config::cascade_configs(&self.config_paths)?;
-        if self.config != new_conf {
-            let new_model = build_model(&new_conf)?;
-            self.model = new_model;
-            self.config = new_conf;
+}
+
+fn xdg_sys_config_dirs() -> impl Iterator<Item = PathBuf> {
+    let xdg_config_dirs_raw = match std::env::var("XDG_CONFIG_DIRS") {
+        Err(std::env::VarError::NotPresent) => std::iter::once("/etc/xdg".to_string()).right(),
+        Err(std::env::VarError::NotUnicode(_raw)) => {
+            todo!();
         }
-        Ok(())
-    }
-    fn build_audio_stream(&mut self) -> Result<buffer::AudioReciever, error::AssistantRsError> {
-        let sample_rate = self.model.get_sample_rate();
-        let host = cpal::default_host();
-        let dev = host
-            .default_input_device()
-            .ok_or(error::AssistantRsError::MicrophoneNotFound)?;
-        let stream_conf = cpal::StreamConfig {
-            channels: 1,
-            sample_rate: cpal::SampleRate(sample_rate as u32),
-        };
-        buffer::AudioReciever::construct(&dev, &stream_conf)
-    }
-    fn run_command(&self, cmd: &Command) -> Result<(), error::AssistantRsError> {
-        let raw_command = cmd.command();
-        process::Command::new("sh")
-            .arg("-c")
-            .arg(raw_command)
-            .stderr(process::Stdio::null())
-            .stdout(process::Stdio::null())
-            .stdin(process::Stdio::null())
-            .spawn()?;
-        Ok(())
-    }
-    pub fn run(&mut self) -> Result<(), error::AssistantRsError> {
-        let sample_rate = self.model.get_sample_rate();
-        let audio_recv = self.build_audio_stream()?;
-        let strm = self.model.create_stream()?;
-        let mut loader = SpeechLoader::new(strm, sample_rate as u32);
-        loop {
-            if loader.time_since_change() > Duration::from_millis(100)
-                && !loader.current_text().is_empty()
-            {
-                break;
-            }
-            let l = audio_recv.wait_until(sample_rate as usize)?;
-            loader.push(&l)?;
-        }
-        println!("======== LOADER DEINIT ==============");
-        let final_samples = loader.num_samples();
-        let (final_msg, _) = loader.finish();
-        let final_msg = final_msg.trim();
-        let phones = speech::Utterance::parse_with_unknowns(final_msg);
-        println!("-> Sample count: {}", final_samples);
-        println!("-> Final text: {}", final_msg);
-        println!("-> Final phones: {:?}", phones);
-        let (cmd, d) = self
-            .config
-            .commands
-            .iter()
-            .map(|cmd| {
-                let dl = metrics::leven_dist(cmd.message(), final_msg);
-                println!("= = > CMD {}: {}", cmd.message(), dl);
-                (cmd, dl)
-            })
-            .min_by(|(_, da), (_, db)| da.partial_cmp(db).unwrap())
-            .unwrap();
-        println!("= > Choice: {}, distance {}", cmd.message(), d);
-        self.run_command(cmd)?;
-        Ok(())
-    }
+        Ok(s) => s.split_owned(":").left(),
+    };
+    let xdg_config_dirs = xdg_config_dirs_raw.map(|s| PathBuf::from(s));
+    xdg_config_dirs
 }
